d4cd9553e3f9968662c91c0b234d6121a12d9e69
leosavioli2019 <78567822+leosavioli2019@users.noreply.github.com>
2021-04-10 12:12:20 -0300

    Add files via upload

---
+++ TeddyMark/text.py
@@ -0,0 +1,13 @@
+def copright():
+	print("TeddyMark, By Leonard Fuzzo Savioli")
+	print("©TeddyMark Lang")
+	print("Type compile to compile your program")
+	print("--------------------------")
+
+def imperative():
+	print("TeddyMark imperative mode")
+	print("Coprighty ©TeddyMark Lang")
+	print("--------------------------")
+
+def license():
+	print(open("license.txt", 'r').read())
